aa33b3e2e766d1460593b0ce8d50f6190e4c7419
irg1008 <ivansudevlop@gmail.com>
2022-06-24 17:26:19 +0200

    Moved evything inside svelte micro island

--- front/astro/src/components/file-drop.svelte
+++ front/astro/src/components/file-drop.svelte
@@ -4,9 +4,9 @@
 
 	const videoType = "video/mp4";
 
+	let videoUrl: string;
 	let dragCounter = 0;
 	let isDragging = false;
-	let video: File;
 	let error: string = "";
 
 	const setDropEffect = (e: DragEvent) => {
@@ -16,7 +16,7 @@
 
 	const setDragEffect = (e: DragEvent) => {
 		if (!e.dataTransfer) return;
-		e.dataTransfer.effectAllowed = "copy";
+		e.dataTransfer.effectAllowed = "all";
 	};
 
 	const onDragIn = (_: DragEvent) => {
@@ -34,14 +34,14 @@
 	const onDrop = (e: DragEvent) => {
 		onDragOut(e);
 		const file = e.dataTransfer?.files?.[0];
+
 		if (file?.type !== videoType) {
 			error = "Only MP4 files are supported";
 			return;
 		}
+
 		error = "";
-		video = file;
-		// video is noot being added if too short or for some other error.
-		// some mp4 files work other don't
+		videoUrl = URL.createObjectURL(file);
 	};
 </script>
 
@@ -53,24 +53,32 @@
 	on:drop|preventDefault={onDrop}
 />
 
+<h1 class="capitalize text-4xl text-neutral-400 text-center leading-relaxed">
+	Drag and drop your video here
+</h1>
+
 {#if error}
-	{error}
+	<span>{error}</span>
 {/if}
 
-{#if video}
-	<h2 class="text-lg font-semibold">Playing {video.name}</h2>
-	<video autoplay controls src={URL.createObjectURL(video)} type={videoType} />
+{#if videoUrl}
+	{#key videoUrl}
+		<video autoplay controls>
+			<source src={videoUrl} type={videoType} />
+			<track kind="captions" />
+		</video>
+	{/key}
 {/if}
 
 {#if isDragging}
 	<div
 		transition:fade={{ duration: 300, easing: cubicOut }}
-		class="fixed h-full w-full top-0 left-0 bg-neutral-50/50 grid place-content-center backdrop-blur-md"
+		class="fixed h-full w-full top-0 left-0 bg-neutral-200/50 grid place-content-center backdrop-blur-md"
 	>
 		<h1
 			in:fly={{ duration: 200, easing: backOut, y: -100 }}
 			out:scale={{ duration: 200, easing: backIn }}
-			class="uppercase font-bold text-center text-8xl"
+			class="uppercase font-bold text-center text-8xl text-neutral-600"
 		>
 			Now drop it!
 		</h1>
