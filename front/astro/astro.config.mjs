2e670215741cada6e50c8a568a6c33c9c3b11ae2
irg1008 <ivansudevlop@gmail.com>
2022-06-24 21:27:05 +0200

    Added adapter for vercel front deployment

--- front/astro/astro.config.mjs
+++ front/astro/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), react()]
+	integrations: [svelte(), tailwind(), react()],
+	adapter: vercel(),
 });
