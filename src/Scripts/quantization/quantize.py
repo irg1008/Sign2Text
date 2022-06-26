2b6bf3d7afe44ca819afb678cbe6bbe4e8463f86
irg1008 <ivansudevlop@gmail.com>
2022-06-26 13:54:28 +0200

    Added docstring on quantization

--- src/Scripts/quantization/quantize.py
+++ src/Scripts/quantization/quantize.py
@@ -8,11 +8,18 @@ from onnxruntime.quantization import quantize_dynamic, QuantType
 
 
 def quantize_onnx_model(onnx_model_path):
+    """Quantize an ONNX model.
+
+    Args:
+        onnx_model_path: Path to the ONNX model.
+    """
     quantized_model_path = onnx_model_path.replace(".onnx", "_quantized.onnx")
 
     # onnx_opt_model = onnx.load(onnx_model_path)
     quantize_dynamic(
-        onnx_model_path, quantized_model_path, weight_type=QuantType.QUInt8
+        onnx_model_path,
+        quantized_model_path,
+        weight_type=QuantType.QUInt8,
     )
 
     print(f"quantized model saved to: {quantized_model_path}")
